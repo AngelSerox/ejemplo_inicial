7b0ce62e467e992dd63bfb7f2efc05ecd8925989
AngelSerox <cmiguelangel105@gmail.com>
2022-09-16 12:06:07 -0500

    Configuracion del Archivo app.js, midelware

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const log = require('morgan')
+const log = require('morgan');
 const app = express();
+const bidyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 //Escuchar Servidor
 app.set('port', process.env.PORT || 3000);
 
 //midelware
-app.use(log('dev'))
+app.use(log('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.listen(app.get('port'),() =>{
     console.log('Servidor funcionando en el puerto', app.get('port'))
