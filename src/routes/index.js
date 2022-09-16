b9201c9c662b4e23cf149cbd6cc88f9167cedc14
AngelSerox <cmiguelangel105@gmail.com>
2022-09-16 13:23:41 -0500

    parametros

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const routes = express.Router();
 
 routes.get('/', (req, res) =>{
-res.render('index')
+res.render('index.ejs', {
+    nombre: 'El MIKE'
+})
 });
 
 module.exports = routes;
