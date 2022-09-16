3ba48de0c6235f9d63ebdd13e637a0e15d47d2c7
AngelSerox <cmiguelangel105@gmail.com>
2022-09-16 11:39:53 -0500

    Configuracion del Archivo app.js, escuchar servidor

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 //Escuchar Servidor
 app.set('port', process.env.PORT || 3000);
 
-app.listen();
+app.listen(app.get('port',() =>{
+    console.log('Servidor funcionando en el puerto', app.get('port'))
+}));
