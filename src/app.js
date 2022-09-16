be300bd6ac80d781ee865ab27c722ef16bf8c5a0
AngelSerox <cmiguelangel105@gmail.com>
2022-09-16 12:52:16 -0500

    Creacion del archivo index en la rutas

--- src/app.js
+++ src/app.js
@@ -3,12 +3,17 @@ const log = require('morgan');
 const app = express();
 const bodyParser = require('body-parser');
 
+const indexRoutes = require('./routes/index.js');
+
 //Escuchar Servidor
 app.set('port', process.env.PORT || 3000);
 
 //midelware
 app.use(log('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
+
+//Rutas
+app.use('/', indexRoutes);
 
 app.listen(app.get('port'),() =>{
     console.log('Servidor funcionando en el puerto', app.get('port'))
