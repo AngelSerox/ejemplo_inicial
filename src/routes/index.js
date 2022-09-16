823239061ab22c5394d03aa5abcd1f0291cd2f94
AngelSerox <cmiguelangel105@gmail.com>
2022-09-16 14:32:39 -0500

    Boostrap

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const routes = express.Router();
+const model = require('../model/valores')();
 
 routes.get('/', (req, res) =>{
-res.render('index.ejs', {
-    nombre: 'El MIKE'
-})
+res.render('index.ejs');
 });
 
 module.exports = routes;
